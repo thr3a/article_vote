4060239969a870958adfbfdee1d0ed2766a6027c
thr3a <12jk172@ms.dendai.ac.jp>
2019-01-03 17:35:05 +0900

    update: TOPページのデザインを改善

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'sessions#index'
 
-  resources :votes, only: [:index, :create]
+  resources :votes, only: [:index, :create] do
+    collection do
+      get :start
+    end
+  end
   get '/auth/:provider/callback', to: 'sessions#callback'
   get '/logout', to: 'sessions#logout'
 
